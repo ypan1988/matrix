42e607b9f18e06bd6672d51ad287701fd369186d
Yi Pan <ypan1988@gmail.com>
2023-07-16 18:24:57 +0100

    add test for the contructor with raw array and dims

--- matrix_test.cpp
+++ matrix_test.cpp
@@ -88,18 +88,55 @@ void matrix_test_constructor_03(bool print = false) {
   Matrix<double, 1> mat1d(1.0, 4);
   if (print) test_print(mat1d, "mat1d =");
   assert(mat1d(0) == 1.0);
-  assert(mat1d(mat1d.n_elem() - 1) == 1.0);
+  assert(mat1d(3) == 1.0);
 
   Matrix<double, 2> mat2d(2.0, 4, 3);
   if (print) test_print(mat2d, "mat2d =");
   assert(mat2d(0, 0) == 2.0);
-  assert(mat2d(mat2d.n_rows() - 1, mat2d.n_cols() - 1) == 2.0);
+  assert(mat2d(3, 2) == 2.0);
 
   Matrix<double, 3> mat3d(3.0, 4, 3, 2);
   if (print) test_print(mat3d, "mat3d =");
   assert(mat3d(0, 0, 0) == 3.0);
-  assert(mat3d(mat3d.n_rows() - 1, mat3d.n_cols() - 1, mat3d.n_slices() - 1) ==
-         3.0);
+  assert(mat3d(3, 2, 1) == 3.0);
+}
+
+void matrix_test_constructor_04(bool print = false) {
+  std::cout << "[TEST]: Constructs a Matrix with array, n_rows, n_cols and "
+               "n_slices\n";
+
+  const double a1[] = {1, 2, 3, 4};
+  Matrix<double, 1> mat1d(a1, 4);
+  if (print) test_print(mat1d, "mat1d =");
+  assert(mat1d(0) == 1.0);
+  assert(mat1d(1) == 2.0);
+  assert(mat1d(2) == 3.0);
+  assert(mat1d(3) == 4.0);
+
+  const double a2[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  Matrix<double, 2> mat2d(a2, 4, 3);
+  if (print) test_print(mat2d, "mat2d =");
+  assert(mat2d(0, 0) == 1.0);
+  assert(mat2d(1, 0) == 2.0);
+  assert(mat2d(2, 0) == 3.0);
+  assert(mat2d(3, 0) == 4.0);
+  assert(mat2d(0, 2) == 9.0);
+  assert(mat2d(1, 2) == 10.0);
+  assert(mat2d(2, 2) == 11.0);
+  assert(mat2d(3, 2) == 12.0);
+
+  const double a3[] = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12,
+                       13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+  Matrix<double, 3> mat3d(a3, 4, 3, 2);
+  if (print) test_print(mat3d, "mat3d =");
+  assert(mat3d(0, 0, 0) == 1.0);
+  assert(mat3d(1, 0, 0) == 2.0);
+  assert(mat3d(2, 0, 0) == 3.0);
+  assert(mat3d(3, 0, 0) == 4.0);
+  assert(mat3d(0, 2, 1) == 21.0);
+  assert(mat3d(1, 2, 1) == 22.0);
+  assert(mat3d(2, 2, 1) == 23.0);
+  assert(mat3d(3, 2, 1) == 24.0);
 }
 
 int main() {
@@ -107,6 +144,7 @@ int main() {
   matrix_test_constructor_01(print_flag);
   matrix_test_constructor_02(print_flag);
   matrix_test_constructor_03(print_flag);
+  matrix_test_constructor_04(print_flag);
 
   return 0;
 }
